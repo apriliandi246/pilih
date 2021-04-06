681e77ea09cb02246c1d407c5bfee9ccd3ff2090
Farhan Apriliandi <apriliandi642@gmail.com>
2021-04-06 16:37:42 +0700

    add join vote functionality

--- components/VoteInputId.jsx
+++ components/VoteInputId.jsx
@@ -1,16 +1,45 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
+import fire from "../config/config";
+import Alert from "./Alert";
 import styles from "../styles/form.module.css";
 
 export default function VoteInput({ isSubmit, onSubmit }) {
+   const router = useRouter();
    const [idVote, setIdVote] = useState("");
+   const [isNotif, setIsNotif] = useState(false);
 
    function submitForm(event) {
       event.preventDefault();
       onSubmit(true);
+
+      fire
+         .firestore()
+         .collection("votes")
+         .doc(idVote.trim())
+         .get()
+         .then((querySnapshot) => {
+            if (querySnapshot.exists === true) {
+               onSubmit(false);
+               router.push(`/vote/${idVote}`);
+            }
+
+            if (querySnapshot.exists === false) {
+               onSubmit(false);
+               setIsNotif(true);
+            }
+         })
+         .catch((err) => {
+            console.log(err);
+         });
    }
 
    return (
       <div className={styles.container}>
+         {isNotif === true && (
+            <Alert onCloseAlert={setIsNotif} message="Vote not found" />
+         )}
+
          <form onSubmit={submitForm}>
             <div className={styles.input}>
                <label htmlFor="voteid">Vote ID *</label>
